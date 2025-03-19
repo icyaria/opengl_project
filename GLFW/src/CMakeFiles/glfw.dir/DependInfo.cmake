9b703c272a12ae8b80af7497cf9f0ca49686fe4e
icyaria <100887481+icyaria@users.noreply.github.com>
2025-03-20 00:03:49 +0200

    Add files via upload

---
+++ GLFW/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/context.c" "GLFW/src/CMakeFiles/glfw.dir/context.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/context.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/egl_context.c" "GLFW/src/CMakeFiles/glfw.dir/egl_context.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/egl_context.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/init.c" "GLFW/src/CMakeFiles/glfw.dir/init.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/init.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/input.c" "GLFW/src/CMakeFiles/glfw.dir/input.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/input.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/monitor.c" "GLFW/src/CMakeFiles/glfw.dir/monitor.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/monitor.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/null_init.c" "GLFW/src/CMakeFiles/glfw.dir/null_init.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/null_init.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/null_joystick.c" "GLFW/src/CMakeFiles/glfw.dir/null_joystick.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/null_joystick.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/null_monitor.c" "GLFW/src/CMakeFiles/glfw.dir/null_monitor.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/null_monitor.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/null_window.c" "GLFW/src/CMakeFiles/glfw.dir/null_window.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/null_window.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/osmesa_context.c" "GLFW/src/CMakeFiles/glfw.dir/osmesa_context.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/osmesa_context.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/platform.c" "GLFW/src/CMakeFiles/glfw.dir/platform.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/platform.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/vulkan.c" "GLFW/src/CMakeFiles/glfw.dir/vulkan.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/vulkan.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/wgl_context.c" "GLFW/src/CMakeFiles/glfw.dir/wgl_context.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/wgl_context.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_init.c" "GLFW/src/CMakeFiles/glfw.dir/win32_init.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_init.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_joystick.c" "GLFW/src/CMakeFiles/glfw.dir/win32_joystick.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_joystick.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_module.c" "GLFW/src/CMakeFiles/glfw.dir/win32_module.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_module.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_monitor.c" "GLFW/src/CMakeFiles/glfw.dir/win32_monitor.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_monitor.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_thread.c" "GLFW/src/CMakeFiles/glfw.dir/win32_thread.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_thread.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_time.c" "GLFW/src/CMakeFiles/glfw.dir/win32_time.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_time.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/win32_window.c" "GLFW/src/CMakeFiles/glfw.dir/win32_window.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/win32_window.c.obj.d"
+  "C:/Users/Maria/Desktop/uni/graphics_project_2425/GLFW/src/window.c" "GLFW/src/CMakeFiles/glfw.dir/window.c.obj" "gcc" "GLFW/src/CMakeFiles/glfw.dir/window.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
